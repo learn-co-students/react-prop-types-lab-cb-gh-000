dd464c2701b8023d57ac51bab40a761463906938
charlyjazz <carlosjazzc1@gmail.com>
2017-10-29 21:55:49 -0400

    Done

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,35 @@
-// Code Product Component Here
+import React from 'react';
+import PropTypes from 'prop-types'
+
+class Product extends React.Component {
+  render() {
+    return React.createElement('button', {}, 'Click me!');
+  }
+}
+
+Product.defaultProps = {
+  hasWatermark: false
+};
+
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  producer: PropTypes.string,
+  hasWatermark: PropTypes.bool.isRequired,
+  color: PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
+  weight: function (props, propName, componentName) {
+    if (((Number.isInteger(props[propName]))) && (props[propName] < 80 || props[propName] > 300)) {
+        return new Error(
+          'Invalid prop `' + propName + '` supplied to' +
+          ' `' + componentName + '`. Validation failed.'
+        );
+    }
+    else {
+      PropTypes.checkPropTypes({
+        weight: PropTypes.number.isRequired
+      }, {weight: props[propName]}, 'prop', 'Product');      
+    }
+  }
+};
+
+
+export default Product;
