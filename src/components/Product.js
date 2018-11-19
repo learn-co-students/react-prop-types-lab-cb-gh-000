a266dca4dad4239e3ee33e6bfdd666fd152613f9
Susanta Biswas <susantasky@gmail.com>
2018-11-19 06:40:22 +0000

    Done.

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,40 @@
 // Code Product Component Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class Product extends React.Component{
+  render(){
+    return(
+      <div className="product">
+        <p>Name: {this.props.name}</p>
+        <p>Producer: {this.props.producer}</p>
+        <p>hasWatermark: {this.props.hasWatermark?'True':'False'}</p>
+        <p>Color: {this.props.color}</p>
+        <p>Weight: {this.props.weight}</p>
+      </div>
+    );
+  }
+}
+
+Product.defaultProps = {
+  hasWatermark:false
+}
+
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  producer: PropTypes.string,
+  hasWatermark: PropTypes.bool,
+  color: PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
+  weight: checkWeight
+}
+
+function checkWeight(props, propName, componentName){
+  let val = props[propName];
+  if(val === undefined)
+    return Error('Number not given');
+  if(typeof(val) !== 'number')
+    return Error('Not a number');
+  if(val < 80 || val > 300)
+    return Error('Out of range');
+  return null;
+}
