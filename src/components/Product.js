16026b05635046a5dbb2829a53bb0ea7bc8f2f07
Bianca Zanardi <bianca.zanardi@gmail.com>
2018-12-30 14:22:34 +0000

    Automatically backed up by Learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,37 @@
-// Code Product Component Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Product extends React.Component {
+
+  render() {
+    return (
+      <div className="order">
+        <ul>
+          <li>{this.props.cone ? 'Cone' : 'Cup'}</li>
+          <li>{this.props.size}</li>
+          <li>{this.props.scoops.length} scoops: {this.props.scoops.join(', ')}</li>
+          <li>Ordered by {this.props.orderInfo.customerName} at {this.props.orderInfo.orderedAt}.</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+Product.defaultProps = {
+  hasWatermark: false
+};
+
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  producer: PropTypes.string,
+  hasWatermark: PropTypes.bool,
+  color: PropTypes.oneOf(['white', 'eggshell-white', 'salmon']),
+  weight: function(props, propName, componentName) {
+   if(props[propName] < 80 || props[propName] > 300) {
+      return new Error('Invalid prop `' + propName + '` supplied to' +
+        ' `' + componentName + '`. Validation failed.');
+   }
+ }
+};
+
+export default Product;
